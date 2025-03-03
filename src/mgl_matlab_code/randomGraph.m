c6fb8247b965859ecf24a213524c96333a2e07f2
AyeshaEkanayaka <66921242+AyeshaEkanayaka@users.noreply.github.com>
2025-03-02 23:49:03 -0500

    Delete src directory

--- src/mgl_matlab_code/randomGraph.m
+++
@@ -1,8 +0,0 @@
-function Tran = randomGraph(p,n)
-Tran  = tril(ones(p,p),-1)';
-idx = find(Tran>0);
-len = numel(idx);
-eidx = randsample(len,n);
-Tran=zeros(p,p);
-Tran(idx(eidx))=1;
-Tran = Tran+Tran';
